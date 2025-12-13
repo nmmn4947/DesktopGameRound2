a6fc4848930f7306ca17b3eff521a00644d5ace7
nmmn4947 <nmmn4947@gmail.com>
2025-12-13 12:54:04 -0800

    Set up a little

--- Assets/Script/IClickable.cs
+++
@@ -1,9 +0,0 @@
-public interface IClickable
-{
-    void OnMouseClick();
-    void OnMouseHold();
-    void OnMouseRelease();
-/*    void OnMiddleMouseClick();*/
-    void OnMiddleMouseHold();
-    void OnMiddleMouseRelease();
-}
---
+++ Assets/Script/Transparent/IClickable.cs
@@ -0,0 +1,9 @@
+public interface IClickable
+{
+    void OnMouseClick();
+    void OnMouseHold();
+    void OnMouseRelease();
+/*    void OnMiddleMouseClick();*/
+    void OnMiddleMouseHold();
+    void OnMiddleMouseRelease();
+}
